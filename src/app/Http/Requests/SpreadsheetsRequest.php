fb52a364df8c035ea6206a5d64480aede40ca2c0
kazu24z <progra.taro2021@gmail.com>
2022-01-31 21:31:36 +0900

    ファイル名を修正

--- src/app/Http/Requests/SpreadsheetsRequest.php
+++ src/app/Http/Requests/SpreadsheetsRequest.php
@@ -6,7 +6,7 @@ use Illuminate\Foundation\Http\FormRequest;
 use Illuminate\Contracts\Validation\Validator;
 use Illuminate\Http\Exceptions\HttpResponseException;
 
-class SpreadsheetsRequest extends ApiErrorRequest
+class SpreadsheetsRequest extends ApiRequest
 {
     /**
      *  Spreadsheetリソースのバリデーションルールを返す
